9f893151794bf8e7f85883746483c21c007e0286
githubsayan7776969 <68245752+githubsayan7776969@users.noreply.github.com>
2023-01-22 18:25:11 +0530

    latest commit

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,71 +1,79 @@
-const HttpError = require("../models/http-error");
-const { validationResult } = require("express-validator");
-const getCoordsForaddress = require("../util/location");
-const Place = require("../models/place");
+const { validationResult } = require('express-validator');
+const mongoose = require('mongoose');
+// const { v4: uuidv4 } = require("uuid");
 
-const { v4: uuidv4 } = require("uuid");
-
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Writers Building",
-    description: "One of the most famous Red buildings in India",
-    location: {
-      lat: 22.5301077,
-      lng: 88.3098528,
-    },
-    address:
-      "Writers Building, H8FX+GGJ, Binoy Badal Dinesh Bag N Rd, Lal Dighi, B.B.D. Bagh, Kolkata, West Bengal 700001",
-    creator: "u1",
-  },
-];
+const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
+const Place = require('../models/place');
+const User = require('../models/user');
 
 const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
+
   let place;
   try {
     place = await Place.findById(placeId);
   } catch (err) {
-    const error = new HttpError("could not find a place", 500);
+    const error = new HttpError(
+      'Something went wrong, could not find a place.',
+      500
+    );
     return next(error);
   }
 
   if (!place) {
-    const error = new HttpError("Place not found", 404);
+    const error = new HttpError(
+      'Could not find a place for the provided id.',
+      404
+    );
     return next(error);
   }
+
   res.json({ place: place.toObject({ getters: true }) });
 };
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  let places;
+
+  // let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
-    const error = new HttpError("fetchin' Failed vehemently!", 500);
+    const error = new HttpError(
+      'Fetching places failed, please try again later',
+      500
+    );
     return next(error);
   }
-  if (!places || places.length === 0) {
+
+  // if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
-      new HttpError("Could not find Places For The Provided User Id", 404)
+      new HttpError('Could not find places for the provided user id.', 404)
     );
   }
+
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map(place =>
+      place.toObject({ getters: true })
+    )
   });
 };
 
 const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError("Validation Error", 422));
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
   }
 
   const { title, description, address, creator } = req.body;
+
   let coordinates;
   try {
-    coordinates = await getCoordsForaddress(address);
+    coordinates = await getCoordsForAddress(address);
   } catch (error) {
     return next(error);
   }
@@ -76,13 +84,37 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image:
-      "https://kolkatatourism.travel/images/places-to-visit/headers/writers-building-kolkata-tourism-entry-fee-timings-holidays-reviews-header.jpg",
-    creator,
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/400px-Empire_State_Building_%28aerial_view%29.jpg',
+    creator
   });
+
+  let user;
   try {
-    await createdPlace.save();
+    user = await User.findById(creator);
   } catch (err) {
-    const error = new HttpError("creating place failed! Try Again", 500);
+    const error = new HttpError('Creating place failed, please try again', 500);
+    return next(error);
+  }
+
+  if (!user) {
+    const error = new HttpError('Could not find user for provided id', 404);
+    return next(error);
+  }
+
+  console.log(user);
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdPlace.save({ session: sess });
+    user.places.push(createdPlace);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(
+      'Creating place failed, please try again.',
+      500
+    );
     return next(error);
   }
 
@@ -92,52 +124,78 @@ const createPlace = async (req, res, next) => {
 const updatePlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
-    res.status(422);
-    throw new HttpError("Validation Error", 422);
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
   }
-  const placeId = req.params.pid;
+
   const { title, description } = req.body;
+  const placeId = req.params.pid;
+
   let place;
   try {
     place = await Place.findById(placeId);
   } catch (err) {
-    const error = new HttpError("Place Not Updated!", 500);
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
     return next(error);
   }
+
   place.title = title;
   place.description = description;
+
   try {
     await place.save();
   } catch (err) {
-    const error = new HttpError("could not update", 500);
+    const error = new HttpError(
+      'Something went wrong, could not update place.',
+      500
+    );
     return next(error);
   }
 
   res.status(200).json({ place: place.toObject({ getters: true }) });
 };
+
 const deletePlace = async (req, res, next) => {
   const placeId = req.params.pid;
+
   let place;
   try {
-    place = await Place.findById(placeId);
-    
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) {
-    const error = new HttpError("could not delete the place", 500);
+    const error = new HttpError(
+      'Something went wrong, could not delete place.',
+      500
+    );
     return next(error);
-    
   }
+
+  if (!place) {
+    const error = new HttpError('Could not find place for this id.', 404);
+    return next(error);
+  }
+
   try {
-    await place.remove();
-    
-  }catch (err) {
-    const error = new HttpError("could not delete the place", 500);
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({ session: sess });
+    place.creator.places.pull(place);
+    await place.creator.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    const error = new HttpError(
+      'Something went wrong, could not delete place.',
+      500
+    );
     return next(error);
-    
   }
-  res.status(200).json({ message: "PLACE-DELETED!!" });
-  // DUMMY_PLACES.splice(index, 1);
+
+  res.status(200).json({ message: 'Deleted place.' });
 };
+
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,7 @@
 const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 const validationResult = require("express-validator").validationResult;
+const User = require("../models/user");
 
 DUMMY_USERS = [
   {
@@ -10,41 +11,84 @@ DUMMY_USERS = [
     password: "test123",
   },
 ];
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError("Fetching failed", 500);
+    return next(error);
+  }
+  res.status(200).json({users:users.map(user => user.toObject({getters: true}))});  
 };
-const SignUp = (req, res, next) => {
+const SignUp = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
     res.status(422);
-    throw new HttpError("Validation Error", 422);
+    return next(new HttpError("Validation Error", 422));
   }
   const { username, email, password } = req.body;
-  const hasUser = DUMMY_USERS.find((user) => user.email === email);
-  if (hasUser) {
-    throw new HttpError("User already exists", 422);
+  // const hasUser = DUMMY_USERS.find((user) => user.email === email);
+  // if (hasUser) {
+  //   throw new HttpError("User already exists", 422);
+  // }
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (err) {
+    const error = new HttpError("Signing Up failed! Please Try Again!", 500);
+    return next(error);
+  }
+  if (existingUser) {
+    const error = new HttpError("User already exists", 422);
+    return next(error);
   }
 
-  const newUser = {
-    id: uuidv4(),
+  const newUser = new User({
     username,
     email,
+    image:
+      "https://www.holidify.com/images/cmsuploads/compressed/20170310204136_20171030202918.jpg",
     password,
-  };
-  DUMMY_USERS.push(newUser);
-  res.status(201).json(newUser);
-};
-const Login = (req, res, next) => {
-  const { email, password } = req.body;
-  const user = DUMMY_USERS.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (!user || user.password !== password) {
-    throw new HttpError("Invalid Credentials", 401);
+    places:[]
+  });
+  try {
+    await newUser.save();
+  } catch (err) {
+    const error = new HttpError("cSigning Up Failed!! Try Again", 500);
+    return next(error);
   }
-  res.status(200).json({ message: "LoggedIn Buddy!" });
+  res.status(201).json({ user: newUser.toObject({ getters: true }) });
 };
+const Login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "Logging in failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "Logged in!" });
+};
+
 exports.getUsers = getUsers;
 exports.SignUp = SignUp;
 exports.Login = Login;
